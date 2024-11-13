74121d311baba770d41fdb8211d562f3cc60e89d
JarinHelliwell <jbrown39@confederationcollege.ca>
2024-11-13 14:32:29 -0500

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function(eleventyConfig) {
+     return {
+          dir: {
+               input: "_src"
+          }
+     }
+}
+// This JavaScript does not need to be linked to our index.html because it is being read by eleventy
